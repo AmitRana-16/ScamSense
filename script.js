0e182237d6cb9f543b16e27ea6e287d4257c14a5
Amit Rana <AmitRana-16>
2025-12-14 19:14:01 +0530

    Further changes

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-function checkAnswer(selected) {
-  const correctAnswer = "AI"; // change per level
-  const result = document.getElementById("result");
+const app = document.querySelector(".app");
+const userNameDisplay = document.getElementById("userNameDisplay");
 
-  if (selected === correctAnswer) {
-    result.textContent = "✅ Correct! This is AI-generated.";
-    result.style.color = "green";
-  } else {
-    result.textContent = "❌ Incorrect. Try again!";
-    result.style.color = "red";
-  }
+let currentScreen = 0;
+
+function updateScreen() {
+  app.style.transform = `translateX(-${currentScreen * 100}vw)`;
 }
+
+function goToNext() {
+  currentScreen = 1;
+  updateScreen();
+}
+
+function goBack() {
+  currentScreen = 0;
+  updateScreen();
+}
+
+function saveName() {
+  const name = document.getElementById("username").value.trim();
+
+  if (!name) {
+    alert("Please enter your name");
+    return;
+  }
+
+  localStorage.setItem("scamSenseUser", name);
+  userNameDisplay.textContent = `👤 Hello ${name}`;
+
+  currentScreen = 2;
+  updateScreen();
+}
+
+/* Load name if already saved */
+window.onload = () => {
+  const savedName = localStorage.getItem("scamSenseUser");
+  if (savedName) {
+    userNameDisplay.textContent = `👤 ${savedName}`;
+  }
+};
